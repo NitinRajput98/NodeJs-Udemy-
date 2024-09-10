3e3a850f613c98db812ae12c8f6bff58cfadf104
Nitin Rajput <nitin98811234@gmail.com>
2024-09-10 11:08:54 +0530

    Parsing request bodies

--- Section-3/app.js
+++ Section-3/app.js
@@ -1,7 +1,9 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // console.log(req.url, req.method, req.headers);
+  const method = req.method;
   const url = req.url;
   if (url === "/") {
     res.write("<html>");
@@ -10,6 +12,21 @@ const server = http.createServer((req, res) => {
       "<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Send</button></form></body>"
     );
     res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+      console.log("Chunk", chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     return res.end();
   }
   //   process.exit();
