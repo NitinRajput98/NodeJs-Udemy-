ece850ce4b11ae21d5a0c04465775a72fdb7fb9d
Nitin Rajput <nitin98811234@gmail.com>
2024-09-19 02:08:27 +0530

    Handling different routes

--- Section-4/app.js
+++ Section-4/app.js
@@ -4,12 +4,28 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("Into my Middleware!");
-  next(); // Allows the request to continue to the next middleware in line!
+// app.use((req, res, next) => {
+//   console.log("Into my Middleware!");
+//   next(); // Allows the request to continue to the next middleware in line!
+// });
+
+app.use("/favicon.ico", (req, res, next) => {
+  res.status(204).end();
 });
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
+  console.log("Always handler executed!");
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  console.log("'/add-product' path handler executed!");
+  res.send("<h1>Add Product Page!</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log(req.url);
+  console.log("'/ path handler executed!'");
   res.send("<h1>This is my page header!</h1>");
 });
 
