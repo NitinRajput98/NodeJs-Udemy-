0c90306f389299137cfa05f0c784d84f084fd5d4
Nitin Rajput <nitin98811234@gmail.com>
2024-09-26 13:08:32 +0530

    Filtering paths

--- Section-4/app.js
+++ Section-4/app.js
@@ -20,7 +20,7 @@ app.use("/favicon.ico", (req, res, next) => {
   res.status(204).end();
 });
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- Section-4/routes/admin.js
+++ Section-4/routes/admin.js
@@ -2,14 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
+// /admin/add-product => get
 router.get("/add-product", (req, res, next) => {
   console.log(req.url);
   console.log("'/ path handler executed!'");
   res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Add product!</button></form>"
+    "<form action='/admin/product' method='POST'><input type='text' name='title'/><button type='submit'>Add product!</button></form>"
   );
 });
 
+// /admin/product => post
 router.post("/product", (req, res, next) => {
   console.log(req.body, "In /product path");
   res.redirect("/");
