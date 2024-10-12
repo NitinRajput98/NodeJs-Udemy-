71238814b41176950720fde2fb8b474da0be3d62
Nitin Rajput <nitin98811234@gmail.com>
2024-10-12 18:12:47 +0530

    Working with EJS

--- Section-4/app.js
+++ Section-4/app.js
@@ -7,15 +7,15 @@ const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const rootDir = require("./utils/path");
 
-const handleHbs = require("express-handlebars");
+// const handleHbs = require("express-handlebars");
 
 const app = express();
 
-app.engine(
-  "handlebars",
-  handleHbs({ layoutsDir: "views/layouts/", defaultLayout: "main-layout" })
-);
-app.set("view engine", "handlebars");
+// app.engine(
+//   "handlebars",
+//   handleHbs({ layoutsDir: "views/layouts/", defaultLayout: "main-layout" })
+// );
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: true }));
