48bac6589000d897daa78cecc2e0f36f97b9cb7d
Nitin Rajput <nitin98811234@gmail.com>
2024-10-16 01:35:06 +0530

    Working on layouts with partials

--- Section-4/app.js
+++ Section-4/app.js
@@ -30,7 +30,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not found" });
+  res.status(404).render("404", { pageTitle: "Page Not found", path: "" });
 });
 
 app.listen(3000);
